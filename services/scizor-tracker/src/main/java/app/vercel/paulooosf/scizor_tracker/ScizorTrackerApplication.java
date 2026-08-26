419de2bb265f4686e6097e680c9bd5a9732e1110
Paulo Henrique <phpessoal2021@gmail.com>
2026-08-25 21:59:14 -0300

    feat: kickoff

---
+++ services/scizor-tracker/src/main/java/app/vercel/paulooosf/scizor_tracker/ScizorTrackerApplication.java
@@ -0,0 +1,13 @@
+package app.vercel.paulooosf.scizor_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScizorTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScizorTrackerApplication.class, args);
+	}
+
+}
