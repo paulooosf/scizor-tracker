d61ef6521ff862fd7390c7dbb2c2d95163859794
Paulo Henrique <phpessoal2021@gmail.com>
2026-08-26 23:25:20 -0300

    feat: configurações iniciais do kafka

---
+++ services/scizor-tracker/src/main/java/app/vercel/paulooosf/scizor_tracker/messaging/TopicosKafka.java
@@ -0,0 +1,13 @@
+package app.vercel.paulooosf.scizor_tracker.messaging;
+
+public final class TopicosKafka {
+
+    public static final String BUG_CRIADO = "bug.criado";
+    public static final String BUG_STATUS_ALTERADO = "bug.status.alterado";
+    public static final String BUG_RESPONSAVEL_ATRIBUIDO = "bug.responsavel.atribuido";
+    public static final String COMENTARIO_ADICIONADO = "comentario.adicionado";
+    public static final String BUG_CRITICO = "bug.critico";
+
+    private TopicosKafka() {
+    }
+}
